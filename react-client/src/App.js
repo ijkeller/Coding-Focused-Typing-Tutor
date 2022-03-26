5440785eb7b325a105e71cd37572e6859ae047a3
isaiah <isaiah.keller@gmail.com>
2022-03-26 08:55:12 -0700

    started initial html and added imports

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Route, Link, Routes, Navigate } from 'react-router-dom';
+
 
 function App() {
+
+  
+
+
+
+
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='title'>Typing Tutor</div>
+      <div class="timer" id="timer">0</div>
+      <div class="container">
+        <div class="code-display" id="codeDisplay">Code</div>
+        <textarea class="user-input" id="userInput" autofocus>Response</textarea>
+      </div>
+
     </div>
   );
 }
