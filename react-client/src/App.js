5ebe44eef05a0e920c38365cb25de24fb9bec5e6
isaiah <isaiah.keller@gmail.com>
2022-03-26 11:37:45 -0700

    imported jsString and set to snippet

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Link, Routes, Navigate } from 'react-router-dom';
+import { jsString } from './components/snippet';
 
 
 function App() {
 
-  
-
-
-
-
-
+  let jsSubString = jsString.substring(0, 294)
 
 
   return (
@@ -19,7 +15,7 @@ function App() {
       <div className='title'>Typing Tutor</div>
       <div class="timer" id="timer">0</div>
       <div class="container">
-        <div class="code-display" id="codeDisplay">Code</div>
+        <div class="snippet" id="snippet"> {jsSubString} </div>
         <textarea class="user-input" id="userInput" autofocus>Response</textarea>
       </div>
 
