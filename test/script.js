f97b7bdd5b3d0a1e7dd4e97e7532d7733506c16a
isaiah <isaiah.keller@gmail.com>
2022-03-28 10:50:45 -0700

    added eventlistener to check if input matches snippet

--- test/script.js
+++ test/script.js
@@ -6,6 +6,23 @@
 const snippetElement = document.getElementById('snippet')
 const inputElement = document.getElementById('userInput')
 
+inputElement.addEventListener('input', () => {
+    const arraySnippet = snippetElement.querySelectorAll('span')
+    const arrayInputValue = inputElement.value.split('')
+    arraySnippet.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.add('incorrect')
+            characterSpan.classList.remove('correct')
+        }
+    })
+})
+
+
+
 async function newSnippet() {
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span')
