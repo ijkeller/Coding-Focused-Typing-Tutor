9a7bdcecf2540ea345f8bfe5a0df576aca29be49
isaiah <isaiah.keller@gmail.com>
2022-03-29 09:11:14 -0700

    got test to version 1 mvp and started transfering the code to the react files

--- test/script.js
+++ test/script.js
@@ -5,23 +5,30 @@
 
 const snippetElement = document.getElementById('snippet')
 const inputElement = document.getElementById('userInput')
+const timerElement = document.getElementById('timer')
 
 inputElement.addEventListener('input', () => {
     const arraySnippet = snippetElement.querySelectorAll('span')
     const arrayInputValue = inputElement.value.split('')
+    let correct = true
+
     arraySnippet.forEach((characterSpan, index) => {
         const character = arrayInputValue[index]
         if (character == null) {
             characterSpan.classList.remove('correct')
             characterSpan.classList.remove('incorrect')
+            correct = false
         } else if (character == characterSpan.innerText) {
             characterSpan.classList.add('correct')
             characterSpan.classList.remove('incorrect')
         } else {
             characterSpan.classList.add('incorrect')
             characterSpan.classList.remove('correct')
+            correct = false
         }
     })
+
+    if (correct) newSnippet(), console.log("fini") 
 })
 
 
@@ -35,6 +42,20 @@ async function newSnippet() {
         snippet.appendChild(characterSpan)
     })
     userInput.value = null
+    startTimer()
+}
+
+let startTime
+function startTimer() {
+    timerElement.innerText = 0.0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
 }
 
 newSnippet()
