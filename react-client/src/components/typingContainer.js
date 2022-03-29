f30bfaa82fe1200208feaf4d1d10c1008211b46b
isaiah <isaiah.keller@gmail.com>
2022-03-29 15:35:14 -0700

    working through creating arrays for both the user input and the initial array

--- react-client/src/components/typingContainer.js
+++ react-client/src/components/typingContainer.js
@@ -1,59 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { jsSubString } from './jsSnippet';
 import Stats from './stats';
 
 
-
-
 function TypingContainer() {
 
+    const [userInput, setUserInput] = useState('')
+    const [userInputArray, setUserInputArray] = useState([])
 
-    // inputElement.addEventListener('input', () => {
-    //     const arraySnippet = snippetElement.querySelectorAll('span')
-    //     const arrayInputValue = inputElement.value.split('')
-    //     let correct = true
+    const [currentSnippet, setCurrentSnippet] = useState(jsSubString)
+    const [snippetArray, setSnippetArray] = useState([])
 
-    //     arraySnippet.forEach((characterSpan, index) => {
-    //         const character = arrayInputValue[index]
-    //         if (character == null) {
-    //             characterSpan.classList.remove('correct')
-    //             characterSpan.classList.remove('incorrect')
-    //             correct = false
-    //         } else if (character == characterSpan.innerText) {
-    //             characterSpan.classList.add('correct')
-    //             characterSpan.classList.remove('incorrect')
-    //         } else {
-    //             characterSpan.classList.add('incorrect')
-    //             characterSpan.classList.remove('correct')
-    //             correct = false
-    //         }
-    //     })
+    setSnippetArray(currentSnippet.split(''))
+    console.log(snippetArray)
 
-    //     if (correct) newSnippet(), console.log("fini")
-    // })
-
-
-    // async function newSnippet() {
-    //     const snippetText = snippetElement.innerText
-    //     snippetElement.innerHTML = ''
-    //     snippetText.split('').forEach(character => {
-    //         const characterSpan = document.createElement('span')
-    //         characterSpan.innerText = character
-    //         snippet.appendChild(characterSpan)
-    //     })
-    //     userInput.value = null
-    //     startTimer()
-    // }
-
-    // newSnippet()
-
-
+    const handleSubmit = (e) => {
+        console.log('handleSubmit')
+        setSnippetArray(currentSnippet.split(''))
+        console.log(snippetArray)
+    }
+    
+    
+    const handleChange = (e) => {
+        const textAreaInput = e.target.value
+        setUserInput(textAreaInput)
+        setUserInputArray(userInput.split(''))
+    }
+    
+     
     return (
 
         <div className="card" id="typing-container">
             <Stats />
-            <div className="snippet" id="snippet">Mauris congue euismod metus non laoreet. Phasellus tempus convallis sem eu varius. In interdum mollis sem. Phasellus id erat a est blandit finibus. Morbi facilisis pulvinar lacus, in ultrices lorem suscipit sit amet. </div>
-            <textarea className="user-input" id="userInput" spellcheck="false" placeholder='Start Typing...' onClick=''></textarea>
+            <div className="snippet" id="snippet"> { jsSubString } </div>
+            
+            <textarea className="user-input" id="userInput" spellCheck="false" placeholder='Start Typing...' onChange={handleChange}></textarea>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
 
     )
