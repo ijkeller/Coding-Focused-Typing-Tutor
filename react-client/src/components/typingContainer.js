8b76ff20a767dc63b419af66b1ae638dd2d58dea
isaiah <isaiah.keller@gmail.com>
2022-03-29 18:35:00 -0700

    both user input and snippet useStates are converted to arrays, working on creating a check that adds a class to the individual letters in the snippetArray

--- react-client/src/components/typingContainer.js
+++ react-client/src/components/typingContainer.js
@@ -9,22 +9,33 @@ function TypingContainer() {
     const [userInputArray, setUserInputArray] = useState([])
 
     const [currentSnippet, setCurrentSnippet] = useState(jsSubString)
-    const [snippetArray, setSnippetArray] = useState([])
+    const [snippetArray, setSnippetArray] = useState([currentSnippet.split('')])
 
-    setSnippetArray(currentSnippet.split(''))
-    console.log(snippetArray)
-
-    const handleSubmit = (e) => {
-        console.log('handleSubmit')
-        setSnippetArray(currentSnippet.split(''))
-        console.log(snippetArray)
-    }
-    
     
     const handleChange = (e) => {
         const textAreaInput = e.target.value
         setUserInput(textAreaInput)
         setUserInputArray(userInput.split(''))
+        // const snippetArraySpan = snippetArray.querySelectorAll('span')
+        let correct = true
+
+        snippetArray.forEach((characterSpan, index) => {
+            const character = userInputArray[index]
+            // if (character == null) {
+            //     characterSpan.className.remove('correct')
+            //     characterSpan.className.remove('incorrect')
+            //     correct = false
+            // } else 
+            if (character == characterSpan.innerText) {
+                characterSpan.className='correct'
+            } else {
+                characterSpan.className='incorrect'
+                correct = false
+            }
+        })
+
+
+
     }
     
      
@@ -35,7 +46,6 @@ function TypingContainer() {
             <div className="snippet" id="snippet"> { jsSubString } </div>
             
             <textarea className="user-input" id="userInput" spellCheck="false" placeholder='Start Typing...' onChange={handleChange}></textarea>
-            <button onClick={handleSubmit}>Submit</button>
         </div>
 
     )
