d654c24127515f98bcae2383ea1448cab5265386
isaiah <isaiah.keller@gmail.com>
2022-04-01 10:15:34 -0700

    troubleshooting now.  console.logging each of the individual variables in different portions of the code.

--- react-client/src/components/typingContainer.js
+++ react-client/src/components/typingContainer.js
@@ -1,56 +1,74 @@
 import React, { useState } from 'react';
 import { jsSubString } from './jsSnippet';
 import Stats from './stats';
+import Span from './span';
 
 
 function TypingContainer() {
 
-    const [userInput, setUserInput] = useState('')
-    const [userInputArray, setUserInputArray] = useState([])
+    let userInput = ''
+
+    const [correctClass, setCorrectClass] = useState(false)
+    const [incorrectClass, setIncorrectClass] = useState(false)
 
     const [currentSnippet, setCurrentSnippet] = useState(jsSubString)
 
-    console.log('---------------forSnippetArray------------')
-    console.log('currentSnippet: ' + currentSnippet)
-    const forSnippetArray = []
-    console.log(currentSnippet.length)
-    for (let i = 0; i <= currentSnippet.length; i++) {
-        forSnippetArray.push(`<span>${currentSnippet[i]}</span>`)
-    }
-    console.log('forSnippetArray.length: ' + forSnippetArray.length)
-    console.log('forSnippetArray: ' + forSnippetArray)
-    console.log('---------------forSnippetArray------------')
+    const snippetArray = currentSnippet.split('').map((snippet, i) => (
+            <Span
+                correctClass={correctClass}
+                incorrectClass={incorrectClass}
+                key={i}
+                snippetCharacter={snippet}
+            />)
+    )
 
-
+    console.log('before handleChange: currentSnippet[0]: ' + currentSnippet[0]) // f
+    console.log('before handleChange: snippetArray[0]: ' + snippetArray[0]) // [object object]
+    console.log('-----------------')
+    
     const handleChange = (e) => {
         const textAreaInput = e.target.value
-        setUserInput(textAreaInput)
-        setUserInputArray(userInput.split(''))
+        // console.log('in handleChange: textAreaInput: ' + textAreaInput) // asd
+        userInput = textAreaInput
+        console.log('in handleChange: userInput: ' + userInput) // asd
+        console.log('in handleChange: userInput[0]: ' + userInput[0]) // asd
+        const splitInput = userInput.split('')
+        // console.log("in handleChange: userInput.split(''): " + userInput.split('')) // a,s,d
+        console.log('in handleChange: splitInput: ' + splitInput) // a,s,d
+        console.log('in handleChange: splitInput[0]: ' + splitInput[0]) // a
+        console.log('in handleChange: splitInput[2]: ' + splitInput[2]) // d
+        console.log('***********')
 
-        // const snippetArraySpan = snippetArray.querySelectorAll('span')
         let correct = true
 
-        forSnippetArray.forEach((snippetCharacter, index) => {
-            const character = userInputArray[index]
-            if (character == null) {
-                // snippetCharacter.removeClass('correct')
-                // snippetCharacter.removeClass('incorrect')
+        snippetArray.forEach((snippetCharacter, index) => {
+            console.log('in handleChange/snippetArray.for Each: snippetCharacter: ' + snippetCharacter) // [object object]
+            console.log('in handleChange/snippetArray.for Each: snippetCharacter.innerText: ' + snippetCharacter.innerText) // undefined
+            console.log('in handleChange/snippetArray.for Each: snippetCharacter.value: ' + snippetCharacter.value) // undefined
+            const userCharacter = userInput[index]
+            console.log('in handleChange/snippetArray.for Each: userCharacter = ' + userCharacter) // 
+            console.log('+++++++++++++++++')
+            if (userCharacter == undefined) {
+                setCorrectClass(false)
+                setIncorrectClass(false)
                 correct = false
-            } else if (character == snippetCharacter.innerText) {
-                snippetCharacter.addClass('correct')
+            } else if (userCharacter == snippetCharacter) {
+                setCorrectClass(true)
+                setIncorrectClass(false)
             } else {
-                snippetCharacter.addClass('incorrect')
+                setCorrectClass(false)
+                setIncorrectClass(true)
                 correct = false
             }
-        })
 
+        })
     }
 
     return (
 
         <div className="card" id="typing-container">
             <Stats />
-            <div className="snippet" id="snippet"> {jsSubString} </div>
+            <div className="snippet" id="snippet"> {snippetArray} </div>
 
             <textarea className="user-input" id="userInput" spellCheck="false" placeholder='Start Typing...' onChange={handleChange}></textarea>
         </div>
