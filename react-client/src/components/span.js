c7d5f28e9948922eb26f65789c4ccea431b97cd6
isaiah <isaiah.keller@gmail.com>
2022-04-02 11:10:19 -0700

    reworking the structure of the span and typingContainer

--- react-client/src/components/span.js
+++ react-client/src/components/span.js
@@ -1,15 +1,59 @@
-
+import React, { useState } from 'react';
+import { jsSubString } from './jsSnippet';
 
 
 function Span(props) {
+
+    const [correctClass, setCorrectClass] = useState(false)
+    const [incorrectClass, setIncorrectClass] = useState(false)
+
+    const [currentSnippet, setCurrentSnippet] = useState(jsSubString)
+
+
+    const snippetArray = currentSnippet.split('')
+
+    let correct = true
+
+    snippetArray.forEach((snippetCharacter, index) => {
+
+        const userCharacter = props.userInput[index]
+
+        console.log('in forEach, userInput[index]: ' + props.userInput[index])
+        console.log('in forEach, userCharacter: ' + userCharacter)
+        console.log('--------------------')
+        
+        
+        
+        if (userCharacter == undefined) {
+            // console.log('(userCharacter == undefined) returned: ' + (userCharacter == undefined))
+
+            correct = false
+        } else if (userCharacter == snippetCharacter) {
+            // console.log('(userCharacter == snippetCharacter) returned: ' + (userCharacter == snippetCharacter))
+
+        } else {
+            // console.log('else ran')
+
+            correct = false
+        }
+
+    })
+
+
+
+
+
+
+
+
 
     return (
 
         <span
             key={props.i}
-            className={
-                `${props.correctClass ? 'correct' : ''}${props.incorrectClass ? 'incorrect' : ''}`
-            }
+            // className={
+            //     `${props.correctClass ? 'correct' : ''}${props.incorrectClass ? 'incorrect' : ''}`
+            // }
             id={`sp${props.id}`} >
             {props.snippetCharacter}
         </span>
--- react-client/src/components/typingContainer.js
+++ react-client/src/components/typingContainer.js
@@ -6,56 +6,39 @@ import Span from './span';
 
 function TypingContainer() {
 
-    let userInput = ''
-
-    const [correctClass, setCorrectClass] = useState(false)
-    const [incorrectClass, setIncorrectClass] = useState(false)
+    
 
     const [currentSnippet, setCurrentSnippet] = useState(jsSubString)
 
     const snippetArray = currentSnippet.split('')
-    
+
+    let userInput = ''
+
     const displaySnippet = snippetArray.map((snippet, i) => (
             <Span
-                correctClass={correctClass}
-                incorrectClass={incorrectClass}
+                // 
                 key={i}
                 id={i}
                 snippetCharacter={snippet}
             />)
     )
     
+    
 
     const handleChange = (e) => {
         const textAreaInput = e.target.value
 
-        userInput = textAreaInput
-        
-        const splitInput = userInput.split('')
+        userInput = textAreaInput.split('')
 
-        let correct = true
+        userInput.map((userCharacter, i) => (
 
-        snippetArray.forEach((snippetCharacter, index) => {
-            const userCharacter = userInput[index]
-            
-             if (userCharacter == undefined) {
-                console.log('(userCharacter == undefined) returned: ' + (userCharacter == undefined))
-                // getElementbyId
-                displaySnippet[index](correctClass(false))
-                displaySnippet[index](incorrectClass(false))
-                correct = false
-            } else if (userCharacter == snippetCharacter) {
-                console.log('(userCharacter == snippetCharacter) returned: ' + (userCharacter == snippetCharacter)) // 
-                displaySnippet[index](correctClass(true))
-                displaySnippet[index](incorrectClass(false))
-            } else {
-                console.log('else ran')
-                displaySnippet[index](correctClass(false))
-                displaySnippet[index](incorrectClass(true))
-                correct = false
-            }
+            <Span
+            userCharacter={userCharacter}
+            />
 
-        })
+        ))
+
+
     }
 
     return (
