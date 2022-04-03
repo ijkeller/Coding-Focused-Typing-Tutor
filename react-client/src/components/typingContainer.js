48164cf37aee2ad7f7be061a982005a3548cc15e
isaiah <isaiah.keller@gmail.com>
2022-04-03 12:54:19 -0700

    when user finishes snippet, the timer resets to 0 (working) and the userInput returns to blank (also working), timer does not start again until the user clicks off and then back on the textarea, this should be fixed once the ternary is fixed.

--- react-client/src/components/typingContainer.js
+++ react-client/src/components/typingContainer.js
@@ -10,6 +10,7 @@ function TypingContainer() {
     const [currentSnippet, setCurrentSnippet] = useState(jsSubString)
     const [userInput, setUserInput] = useState('')
     const [userActive, setUserActive] = useState(false)
+    const [resetTimer, setResetTimer] = useState(false)
 
     // convert current snippet
     const snippetObject = currentSnippet.split('')
@@ -24,29 +25,40 @@ function TypingContainer() {
         />)
     )
 
-    const boolInput = (userInput[0] != undefined)
-    // const onFocus = () => boolInput ? console.log('onFocus says true') : console.log('onFocus, but no input')
-    const onFocus = () => setUserActive(true)
+    
+    // const onFocus = (() => (userInput[0] != undefined) ? console.log('onFucos, user active') : console.log('onFocus, but no input') )
+    const onFocus = () => (setUserActive(true), setResetTimer(false))
     const onBlur = () => setUserActive(false)
+    
 
-    console.log('userActive' + userActive)
+    console.log('resetTimer: ' + resetTimer)
+
+    console.log('userActive: ' + userActive)
     console.log('userInput[0]: ' + userInput[0])
-    console.log('boolInput: ' + boolInput)
-    console.log('boolInput type: ' + typeof boolInput)
 
     const handleChange = (e) => {
 
-        setUserInput(e.target.value.split(''))
+        setUserInput(e.target.value)
+
+        if (userInput.length >= snippetObject.length)
+
+            return (
+
+                setResetTimer(true),
+                setUserActive(false),
+                setUserInput('')
+
+            )
 
     }
 
     return (
 
         <div className="card" id="typing-container">
-            <Stats userActive={userActive} />
+            <Stats userActive={userActive} reset={resetTimer} />
             <div className="snippet" id="snippet"> {displaySnippet} </div>
 
-            <textarea className="user-input" id="userInput" spellCheck="false" placeholder='Start Typing...' onChange={handleChange} onFocus={onFocus} onBlur={onBlur} ></textarea>
+            <textarea className="user-input" id="userInput" spellCheck="false" placeholder='Start Typing...' onChange={handleChange} onFocus={onFocus} onBlur={onBlur} value={userInput}></textarea>
             {console.log('--------------')}
         </div>
 
