09d44ecc4b7e73cae4c1bceac834599b97915e98
isaiah <isaiah.keller@gmail.com>
2022-04-05 08:36:55 -0700

    minor changes to app.js

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Link, Routes, Navigate } from 'react-router-dom';
-import { jsSubString } from './components/jsSnippet';
 import TypingContainer from './components/typingContainer';
 import Nav from './components/nav';
-import Keyboard from './components/keyboard'
-import Stats from './components/stats'
 import QwertyContainer from './components/qwertyContainer'
 
 
@@ -14,8 +11,8 @@ function App() {
 
     <div className="App">
 
-      <div className='title'>FST CDR</div>
-      <Nav />
+      {/* <div className='title'>FST CDR</div> */}
+      {/* <Nav /> */}
       <TypingContainer />
       <QwertyContainer />
 
